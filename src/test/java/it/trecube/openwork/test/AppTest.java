44e6d263383645e84c635648adbe0bbc75fd66bf
Paolo Pino <paolo.pino@3cube.it>
2015-06-05 18:36:36 +0200

    aggiunto in AppTest.java TestCreazione UtenteSu+ e account associato

--- src/test/java/it/trecube/openwork/test/AppTest.java
+++ src/test/java/it/trecube/openwork/test/AppTest.java
@@ -1,6 +1,12 @@
 package it.trecube.openwork.test;
 
-import com.openworkbpm.schema.ServiceResultOfLoginInfo;
+import com.openworkbpm.schema.*;
+import com.openworkbpm.schema.data_application.IDataManagement;
+import com.openworkbpm.schema.data_application.IDataManagementbasicHttpBindingGateway;
+import com.openworkbpm.schema.event_application.IEventManagement;
+import com.openworkbpm.schema.event_application.IEventManagementbasicHttpBindingGateway;
+import com.openworkbpm.schema.identity_application.IIdentityManagement;
+import com.openworkbpm.schema.identity_application.IIdentityManagementbasicHttpBindingGateway;
 import com.openworkbpm.schema.session_application.ISessionManagement;
 import com.openworkbpm.schema.session_application.ISessionManagementbasicHttpBindingGateway;
 import junit.framework.Test;
@@ -9,15 +15,24 @@ import junit.framework.TestSuite;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
-
+import java.util.Date;
 
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
+public class AppTest
+        extends TestCase
 {
+
+    ISessionManagement sessionManagement = new ISessionManagementbasicHttpBindingGateway().getBasicHttpBindingGatewayISessionManagement();
+
+    IIdentityManagement identityManagement =  new IIdentityManagementbasicHttpBindingGateway().getBasicHttpBindingGatewayIIdentityManagement();
+
+    IDataManagement dataManagement = new IDataManagementbasicHttpBindingGateway().getBasicHttpBindingGatewayIDataManagement();
+
+    IEventManagement eventManagement = new IEventManagementbasicHttpBindingGateway().getBasicHttpBindingGatewayIEventManagement();
+
     /**
      * Create the test case
      *
@@ -40,9 +55,58 @@ public class AppTest
      * Rigourous Test :-)
      */
 
-    public void testApp() throws UnknownHostException {
-        ISessionManagement sessionManagement = new ISessionManagementbasicHttpBindingGateway().getBasicHttpBindingGatewayISessionManagement();
+
+    String repositoryId = "0782621F10E744FE9E2DA2B200EFE71D" ;
+    String modelUrlUtenti ="/"+repositoryId+"/Model/8BF7283E735943D780C3A2B3010CDA2A";
+
+
+    public void testCreateUser() throws UnknownHostException {
         ServiceResultOfLoginInfo result = sessionManagement.login("spesautileplustesT", "administrator", "SupTest!", false, InetAddress.getLocalHost().getHostName());
+
         assertFalse(result.isError());
+        RequestInfo requestInfo = new RequestInfo();
+        requestInfo.setToken(result.getValue().getToken());
+
+        ServiceResultOfIdentity identityResult = identityManagement.getNewIdentity(repositoryId, modelUrlUtenti, requestInfo);
+        assertFalse(identityResult.isError());
+        Identity identity = identityResult.getValue();
+        identity.setName("Mario Rossi"); //TODO: serve?
+
+        updateFieldItem(identity.getFields(), "Nome", "Mario");
+
+    
+                
+        updateFieldItem(identity.getFields(), "Nome", "Mario");
+        updateFieldItem(identity.getFields(), "Cognome", "Marii");
+//        updateFieldItem(identity.getFields(),"Data di nascita",new Date()));
+        updateFieldItem(identity.getFields(), "Username", "nomeOrg_Able2_"+new Date().getTime());
+        updateFieldItem(identity.getFields(), "Sesso", "M");
+
+        updateFieldItem(identity.getFields(), "Email", "test@mail.com");
+
+
+        //identity.setFields(fields);
+
+        ServiceResultOfstring createResult = identityManagement.createIdentity(repositoryId, identity, requestInfo);
+        assertFalse(createResult.isError());
+
+
+        //Creazione Account
+        Account userAccount = new Account();
+
+        userAccount.setName("user_"+new Date().getTime());
+        userAccount.setAllowPasswordChange(true);
+        userAccount.setId(createResult.getValue());
+        userAccount.setPasswordRequired(true);
+
+        ServiceResult accountResult = identityManagement.createAccount(repositoryId,userAccount,"password",requestInfo);
+        assertFalse(accountResult.isError());
+    }
+
+    private void updateFieldItem(Fields fields , String key, Object value){
+        for(Fields.Item item : fields.getItem()){
+            if(item.getKey().equals(key))
+                item.setVal(value);
+        }
     }
 }
